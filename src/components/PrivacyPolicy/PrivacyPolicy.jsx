fd6275cd6a2360f8f8ccf5d4bc3a3a07539ca522
Ваше ім'я <constantin161089@gmail.com>
2025-04-03 20:44:16 +0300

    тест стилей

--- src/components/PrivacyPolicy/PrivacyPolicy.jsx
+++ src/components/PrivacyPolicy/PrivacyPolicy.jsx
@@ -5,22 +5,26 @@ const PrivacyPolicy = () => {
   const { t } = useTranslation();
 
   return (
-    <div className={s.privacyPolicyContainer}>
-      <h1 className={s.title}>{t("privacyPolicy.title")}</h1>
+    <div className={s.privacyPolicy}>
+      <h1 className={s.titlePolicy}>{t("privacyPolicy.title")}</h1>
 
-      <section className={s.section}>
-        <h2>1. {t("privacyPolicy.dataCollectionTitle")}</h2>
+      <div className={s.policyList}>
+        <h2 className={s.policyListTitle}>
+          1.{t("privacyPolicy.dataCollectionTitle")}
+        </h2>
         <p>{t("privacyPolicy.dataCollectionDescription")}</p>
         <ul>
-          <li>{t("privacyPolicy.fullName")}</li>
-          <li>{t("privacyPolicy.phoneNumber")}</li>
-          <li>{t("privacyPolicy.comment")}</li>
+          <li>-{t("privacyPolicy.fullName")}</li>
+          <li>-{t("privacyPolicy.phoneNumber")}</li>
+          <li>-{t("privacyPolicy.comment")}</li>
         </ul>
         <p>{t("privacyPolicy.dataUseDescription")}</p>
-      </section>
+      </div>
 
-      <section className={s.section}>
-        <h2>2. {t("privacyPolicy.dataProcessingTitle")}</h2>
+      <div className={s.policyList}>
+        <h2 className={s.policyListTitle}>
+          2.{t("privacyPolicy.dataProcessingTitle")}
+        </h2>
         <p>{t("privacyPolicy.dataProcessingDescription")}</p>
         <p>{t("privacyPolicy.chatBotPolicyDescription")}</p>
         <p>
@@ -34,22 +38,28 @@ const PrivacyPolicy = () => {
           </a>
           .
         </p>
-      </section>
+      </div>
 
-      <section className={s.section}>
-        <h2>3. {t("privacyPolicy.dataUsageTitle")}</h2>
+      <div className={s.policyList}>
+        <h2 className={s.policyListTitle}>
+          3.{t("privacyPolicy.dataUsageTitle")}
+        </h2>
         <p>{t("privacyPolicy.dataUsageDescription")}</p>
-      </section>
+      </div>
 
-      <section className={s.section}>
-        <h2>4. {t("privacyPolicy.dataSecurityTitle")}</h2>
+      <div className={s.policyList}>
+        <h2 className={s.policyListTitle}>
+          4.{t("privacyPolicy.dataSecurityTitle")}
+        </h2>
         <p>{t("privacyPolicy.dataSecurityDescription")}</p>
-      </section>
+      </div>
 
-      <section className={s.section}>
-        <h2>5. {t("privacyPolicy.dataConsentTitle")}</h2>
+      <div className={s.policyList}>
+        <h2 className={s.policyListTitle}>
+          5.{t("privacyPolicy.dataConsentTitle")}
+        </h2>
         <p>{t("privacyPolicy.dataConsentDescription")}</p>
-      </section>
+      </div>
     </div>
   );
 };
